eb22e89b3702380c67b2b198ee140bde3972f606
quigley-c <quigleyc@allegheny.edu>
2019-05-28 20:33:14 -0400

    have nade object use nade image

--- src/objects/nade.lua
+++ src/objects/nade.lua
@@ -8,6 +8,7 @@ local nade_height = 16
 
 return {
     init = function(self)
+	img = love.graphics.newImage('nade.png') 
     end,
     destroy = function(self)
 	obj.create('explosion', {x = self.x, y = self.y})
@@ -18,6 +19,7 @@ return {
     end,
     render = function(self)
         love.graphics.setColor(1, 0, 1, 1)
+	love.graphics.draw(img, self.x, self.y, 0, 1, 1, 0, 32)
         love.graphics.rectangle('line', self.x, self.y, nade_width, nade_height)
     end,
 }
