3c1354890f82d18617a8de6132f857a1f6303f78
Justin Stanley <jtst@iastate.edu>
2019-05-28 19:07:41 -0500

    update object system

--- src/main.lua
+++ src/main.lua
@@ -11,14 +11,14 @@ local player
 
 function love.load()
     world = map.load('test')
-    player = obj.create('player', world.start_x, world.start_y)
+    player = obj.create('player', { x=world.start_x, y=world.start_y })
 end
 
 function love.update(dt)
-    obj.update(player, dt)
+    obj.update_all(dt)
 end
 
 function love.draw()
     map.render(world)
-    obj.render(player)
+    obj.render_all()
 end
--- src/obj.lua
+++ src/obj.lua
@@ -1,29 +1,46 @@
 --[[
     obj.lua
     functions for creating and manipulating generic objects
+
+    the object system maintains its own list of active objects
+    each object is a table with the following fields:
+        __type    : object functable
+        __destroy : destroy flag, once 'true' will be destroyed on next step
 --]]
 
-local obj = {}
+local obj = { object_list = {} }
 
-function obj.create(typename, x, y)
-    otype = require('objects/' .. typename)
+function obj.create(typename, initial)
+    initial.__type = require('objects/' .. typename)
+    initial.__destroy = false
+    initial.__type.init(initial, params)
 
-    local ohandle = { __type = otype, x=x, y=y }
-    otype.init(ohandle)
+    table.insert(obj.object_list, initial)
 
-    return ohandle
+    return initial
 end
 
 function obj.destroy(handle)
+    handle.__destroy = true
     handle.__type.destroy(handle)
 end
 
-function obj.update(handle, dt)
-    handle.__type.update(handle, dt)
+function obj.update_all(dt)
+    for i, v in pairs(obj.object_list) do
+        if v.__destroy then
+            -- drop this object out of the array and proceed
+            v.__type.destroy(v)
+            obj.object_list[i] = nil
+        else
+            v.__type.update(v, dt)
+        end
+    end
 end
 
-function obj.render(handle)
-    handle.__type.render(handle)
+function obj.render_all()
+    for i, v in pairs(obj.object_list) do
+        v.__type.render(v)
+    end
 end
 
 return obj
