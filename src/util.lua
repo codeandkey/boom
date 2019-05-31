f25b7c6b17f0f1a9b98ce322775bfd581605200f
Justin Stanley <jtst@iastate.edu>
2019-05-31 14:09:25 -0500

    util: add documentation

--- src/util.lua
+++ src/util.lua
@@ -5,6 +5,13 @@
 
 local util = {}
 
+--[[
+    util.aabb(a, b)
+
+    Performs an AABB collision test between <a> and <b>.
+    This method expects the members 'x', 'y', 'w', 'h' to be set for each argument.
+--]]
+
 function util.aabb(a, b)
     if a.x + a.w < b.x then return false end
     if b.x + b.w < a.x then return false end
@@ -13,6 +20,13 @@ function util.aabb(a, b)
 
     return true
 end
+
+--[[
+    util.basename(str)
+
+    Equivalent of the POSIX basename(), grabbing the last path element.
+    TODO: this might explode on windows, might need a cross-platform variant
+--]]
 
 function util.basename(str)
     return string.gsub(str, "(.*/)(.*)", "%2")
