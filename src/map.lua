c9a1eafc7ee95e550bffb12b3150b48958b6143b
Justin Stanley <jtst@iastate.edu>
2019-05-31 11:27:41 -0500

    implement tile layer rendering

--- src/map.lua
+++ src/map.lua
@@ -4,6 +4,7 @@
 --]]
 
 local obj = require 'obj'
+local util = require 'util'
 local map = {}
 
 --[[
@@ -12,9 +13,49 @@ local map = {}
 
 function map.load(name)
     map.current = require('assets/maps/' .. name)
+    map.current.tiles = {}
 
-    for k, v in pairs(map.current.objects) do
-        obj.create(v.typename, v.initial)
+    -- for now, only allow one tileset
+    -- this is because batches can only go through one image and dividing
+    -- that will be very painful when loading
+    assert(#map.current.tilesets <= 1)
+
+    -- initialize tilesets
+    for k, v in ipairs(map.current.tilesets) do
+        v.texture = love.graphics.newImage('assets/sprites/' .. util.basename(v.image))
+
+        -- create quads for each tile in the set
+        local cur_tile = v.firstgid
+        local tw, th = v.tilewidth, v.tileheight
+        
+        for y=0,(v.imageheight/v.tileheight)-1 do
+            for x=0,(v.imagewidth/v.tilewidth)-1 do
+                map.current.tiles[cur_tile] = love.graphics.newQuad(x * tw, y * th, tw, th, v.imagewidth, v.imageheight)
+            end
+
+            cur_tile = cur_tile + 1
+        end
+    end
+
+    -- initialize layers
+    for k, v in ipairs(map.current.layers) do
+        if v.type == 'tilelayer' then
+            -- create render batch alongside data
+            v.batch = love.graphics.newSpriteBatch(map.current.tilesets[1].texture, 1000, 'static')
+
+            for y=0,v.height-1 do
+                for x=0,v.width-1 do
+                    local tid = v.data[1 + x + y * v.width]
+
+                    if tid ~= 0 then
+                        v.batch:add(map.current.tiles[tid],
+                                    v.offsetx + x * map.current.tilewidth,
+                                    v.offsety + y * map.current.tileheight)
+                    end
+                end
+            end
+        elseif v.type == 'objectlayer' then
+        end
     end
 end
 
@@ -23,14 +64,10 @@ end
 --]]
 
 function map.render()
-    for y=1,map.current.height do
-        for x=1,map.current.width do
-            local tile_id = map.current.data[x + (y-1) * map.current.width]
-
-            if tile_id ~= 0 then
-                love.graphics.setColor(map.tiles[tile_id].color)
-                love.graphics.rectangle('fill', (x - 1) * tile_width, (y - 1) * tile_height, tile_width, tile_height)
-            end
+    for k, v in ipairs(map.current.layers) do
+        if v.type == 'tilelayer' then
+            love.graphics.draw(v.batch)
+        elseif v.type == 'objectlayer' then
         end
     end
 end
