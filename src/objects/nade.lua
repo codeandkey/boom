195ad01f12694f7f89a3e8e9c4e2ad57050d3ecb
Justin Stanley <jtst@iastate.edu>
2019-06-05 14:50:02 -0500

    nade: fix whitespace

--- src/objects/nade.lua
+++ src/objects/nade.lua
@@ -8,18 +8,18 @@ local nade_height = 16
 
 return {
     init = function(self)
-	img = love.graphics.newImage('nade.png') 
+        img = love.graphics.newImage('nade.png') 
     end,
     destroy = function(self)
-	obj.create('explosion', {x = self.x, y = self.y})
+        obj.create('explosion', {x = self.x, y = self.y})
     end,
     update = function(self, dt)
-    	--logic
-	self.x = self.x + self.velocity * dt
+        --logic
+        self.x = self.x + self.velocity * dt
     end,
     render = function(self)
         love.graphics.setColor(1, 0, 1, 1)
-	love.graphics.draw(img, self.x, self.y, 0, 1, 1, 0, 32)
+        love.graphics.draw(img, self.x, self.y, 0, 1, 1, 0, 32)
         love.graphics.rectangle('line', self.x, self.y, nade_width, nade_height)
     end,
 }
