08748e35c86408f84d775b1037e38bdbb97bed77
quigley-c <quigleyc@allegheny.edu>
2019-06-05 17:30:39 -0400

    add timer for nade fuse

--- src/objects/nade.lua
+++ src/objects/nade.lua
@@ -8,28 +8,37 @@ local nade_height = 16
 
 return {
     init = function(self)
-	      -- consts
-	      img = love.graphics.newImage('assets/sprites/16x16_nade.png')
-    	  self.gravity = self.gravity or 350
+	    -- consts
+	    img = love.graphics.newImage('assets/sprites/16x16_nade.png')
+    	self.gravity = self.gravity or 350
 
       	-- state
+        fuse_time = 100
     end,
 
     destroy = function(self)
-	      obj.create(self.__layer, 'explosion', {x = self.x, y = self.y})
+	    obj.create(self.__layer, 'explosion', {x = self.x, y = self.y})
     end,
 
     update = function(self, dt)
 
-	      -- apply gravity
-	      self.dy = self.dy + self.gravity * dt
+        -- decrement fuse and explode if expired
+        if fuse_time > 0 then
+            fuse_time = fuse_time - 1
+        else
+            fuse_time = 0
+            self.destroy()
+        end
 
-	      -- update position
-	      self.x = self.x + self.dx * dt
-	      self.y = self.y + self.dy * dt
+	    -- apply gravity
+	    self.dy = self.dy + self.gravity * dt
+
+	    -- update position
+	    self.x = self.x + self.dx * dt
+	    self.y = self.y + self.dy * dt
     end,
 
     render = function(self)
-	      love.graphics.draw(img, self.x, self.y + nade_height, 0, 1, 1, 0, 16)
+	    love.graphics.draw(img, self.x, self.y + nade_height, 0, 1, 1, 0, 16)
     end,
 }
