a96240ec1b55d5827b372b1760fca830edd8f1ab
quigley-c <quigleyc@allegheny.edu>
2019-06-05 17:48:28 -0400

    add explosion timer that destroys it when expired

--- src/objects/explosion.lua
+++ src/objects/explosion.lua
@@ -1,19 +1,30 @@
 --[[
-    nade.lua
-    grenade object
+    explosion.lua
+    explosion object
 --]]
 
-local ex_width = 32
-local ex_height = 32
+local obj = require 'obj'
+local width = 32
+local height = 32
 local damaging = true
 
 return {
     init = function(self)
+        self.anim_time = 50
     end,
+
     destroy = function(self)
     end,
+
     update = function(self, dt)
+        if self.anim_time > 0 then
+            self.anim_time = self.anim_time - 1
+        else
+            self.anim_time = 0
+            obj.destroy(self)
+        end
     end,
+
     render = function(self)
         love.graphics.setColor(1, 0, 1, 1)
         love.graphics.rectangle('line', self.x, self.y, ex_width, ex_height)
