aef43a745317555d95f574b79956d59cd8864dd1
quigley-c <quigleyc@allegheny.edu>
2019-06-05 17:08:03 -0400

    change parameters for nade movement once instantiated

--- src/objects/nade.lua
+++ src/objects/nade.lua
@@ -8,19 +8,28 @@ local nade_height = 16
 
 return {
     init = function(self)
-	img = love.graphics.newImage('assets/sprites/16x16_nade.png') 
+	      -- consts
+	      img = love.graphics.newImage('assets/sprites/16x16_nade.png')
+    	  self.gravity = self.gravity or 350
+
+      	-- state
     end,
 
     destroy = function(self)
-	obj.create(self.__layer, 'explosion', {x = self.x, y = self.y})
+	      obj.create(self.__layer, 'explosion', {x = self.x, y = self.y})
     end,
 
     update = function(self, dt)
-    	--logic
-	self.x = self.x + self.velocity * dt
+
+	      -- apply gravity
+	      self.dy = self.dy + self.gravity * dt
+
+	      -- update position
+	      self.x = self.x + self.dx * dt
+	      self.y = self.y + self.dy * dt
     end,
 
     render = function(self)
-	love.graphics.draw(img, self.x, self.y + nade_height, 0, 1, 1, 0, 16)
+	      love.graphics.draw(img, self.x, self.y + nade_height, 0, 1, 1, 0, 16)
     end,
 }
