07382e25562d9e917344c7ed05ea9ce54200d056
Justin Stanley <jtst@iastate.edu>
2019-08-05 09:34:24 -0500

    Add strings API

---
+++ src/strings.lua
@@ -0,0 +1,27 @@
+--- Game string table manager.
+
+local strings = {
+    vals = {},
+}
+
+--- Load a language into the string table.
+-- @param lang Language to load, from strings/<name>.lua
+function strings.load(lang)
+    status, result = pcall(function() return require ('strings/' .. lang) end)
+
+    if status then
+        strings.vals = result
+    else
+        log.error('Error loading strings for language ' .. lang .. '!')
+    end
+end
+
+--- Get a game string.
+-- @param name String index to retrieve.
+-- @return The associated string, or '[ERROR]' if not found.
+function strings.get(index)
+    return strings.vals[index] or '[ERROR]'
+end
+
+strings.load('english')
+return strings
