e735335caa39958abff2288af373a079746f55d2
quigley-c <quigleyc@allegheny.edu>
2019-08-11 16:28:11 -0400

    remove unused variables

---
+++ src/object_types/load_trigger.lua
@@ -0,0 +1,8 @@
+--- Load Trigger object type.
+-- Transitions game between maps when the player interacts with it.
+
+return {
+    init = function(this)
+        destination = this.destination
+    end,
+}
