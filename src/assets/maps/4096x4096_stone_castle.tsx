9802921fc6502ad840d6c74d6a71e5f5a263dccc
quigley-c <quigleyc@allegheny.edu>
2019-08-13 16:18:34 -0400

    update maps to use new tiles

---
+++ src/assets/maps/4096x4096_stone_castle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="4096x4096_stone_castle" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../sprites/4096x4096_stone_castle.png" width="256" height="256"/>
+</tileset>
