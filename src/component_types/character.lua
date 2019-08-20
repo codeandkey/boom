100de283333068f013add00d2243005ad2de5b66
Justin Stanley <jtst@iastate.edu>
2019-08-20 17:06:41 -0500

    character: implement precise aiming

--- src/component_types/character.lua
+++ src/component_types/character.lua
@@ -16,7 +16,8 @@ local util         = require 'util'
     Inputs:
         'left' : move left
         'right' : move right
-        'crouch' : crouch
+        'down' : crouch
+        'up'   : aim up
         'jump' : jump
         'throw' : grenade throw
 
@@ -26,15 +27,17 @@ local util         = require 'util'
 return {
     init = function(this)
         -- Configuration.
-        this.gravity           = this.gravity or 350
-        this.crouch_decel      = this.crouch_decel or 600
-        this.passive_decel     = this.passive_decel or 400
-        this.midair_decel      = this.midair_decel or 200
-        this.jump_dy           = this.jump_dy or -280
-        this.dx_accel          = this.dx_accel or 1600
-        this.dx_max            = this.dx_max or 150
-        this.grenade_dampening = this.grenade_dampening or 3
-        this.color             = this.color or {1, 1, 1, 1}
+        this.gravity            = this.gravity or 350
+        this.crouch_decel       = this.crouch_decel or 600
+        this.passive_decel      = this.passive_decel or 400
+        this.midair_decel       = this.midair_decel or 200
+        this.jump_dy            = this.jump_dy or -280
+        this.dx_accel           = this.dx_accel or 1600
+        this.dx_max             = this.dx_max or 150
+        this.grenade_dampening  = this.grenade_dampening or 6
+        this.color              = this.color or {1, 1, 1, 1}
+        this.throw_pitch_speed  = 20 -- higher values make the player aim up/down faster
+        this.throw_strength     = this.throw_strength or 40
 
         this.w = this.w or 14
         this.h = this.h or 32
@@ -56,6 +59,8 @@ return {
         this.direction     = 'right'
         this.nade          = nil
         this.throw_enabled = false
+        this.throw_pitch   = 0 -- radians
+        this.target_pitch  = 0
     end,
 
     explode = function(this, _, _, _)
@@ -107,6 +112,10 @@ return {
     end,
 
     inputdown = function(this, key)
+        if this.__parent.name == 'player' then
+            log.debug('player.character: inputdown %s', key)
+        end
+
         if key == 'left' then
             this.wants_left = true
         elseif key == 'right' then
@@ -138,10 +147,18 @@ return {
                     object.call(other_obj, 'interact', this.__parent)
                 end
             end)
+        elseif key == 'up' then
+            this.wants_up = true
+        elseif key == 'down' then
+            this.wants_down = true
         end
     end,
 
     inputup = function(this, key)
+        if this.__parent.name == 'player' then
+            log.debug('player.character: inputup %s', key)
+        end
+
         if key == 'left' then
             this.wants_left = false
         elseif key == 'right' then
@@ -149,13 +166,22 @@ return {
         elseif key == 'throw' then
             -- Throw a grenade if we're holding one.
             if this.nade ~= nil then
-                this.nade:throw(this.dx / this.grenade_dampening, this.dy / this.grenade_dampening)
+                local vx, vy = math.cos(this.throw_angle), math.sin(this.throw_angle)
+
+                vx = vx * this.throw_strength + this.dx / this.grenade_dampening
+                vy = vy * this.throw_strength + this.dy / this.grenade_dampening
+
+                this.nade:throw(vx, vy)
                 this.nade = nil
             end
         elseif key == 'jump' then
             if this.dy < 0 then
                 this.dy = this.dy / 2
             end
+        elseif key == 'up' then
+            this.wants_up = false
+        elseif key == 'down' then
+            this.wants_down = false
         end
     end,
 
@@ -259,6 +285,44 @@ return {
         if math.abs(this.dy) > 20 then
             this.jump_enabled = false
         end
+
+        -- Decide what direction the player will aim.
+        -- A directional switch should apply immediately, so we only animate the 'pitch' of the angle.
+    
+        if this.wants_up and not this.wants_down then
+            if this.wants_left or this.wants_right then
+                this.target_pitch = -3.141 / 4.0
+            else
+                this.target_pitch = -3.141 / 2.0
+            end
+        elseif this.wants_down and not this.wants_up then
+            if this.wants_left or this.wants_right then
+                this.target_pitch = 3.141 / 4.0
+            else
+                this.target_pitch = 3.141 / 2.0
+            end
+        else
+            this.target_pitch = 0
+        end
+
+        -- Animate player pitch to target.
+        if this.throw_pitch < this.target_pitch then
+            this.throw_pitch = math.min(this.throw_pitch + dt * this.throw_pitch_speed, this.target_pitch)
+        elseif this.throw_pitch > this.target_pitch then
+            this.throw_pitch = math.max(this.throw_pitch - dt * this.throw_pitch_speed, this.target_pitch)
+        end
+
+        -- Compute actual throw angle.
+        if this.direction == 'right' then
+            this.throw_angle = this.throw_pitch
+        else
+            this.throw_angle = 3.141 - this.throw_pitch
+        end
+
+        -- write stats
+        --if this.__parent.name == 'player' then
+        --    log.debug('target pitch %f, pitch %f, angle %f', this.target_pitch, this.throw_pitch, this.throw_angle)
+        --end
     end,
 
     render = function(this)
@@ -269,6 +333,14 @@ return {
             this.spr = this.spr_idle
         else
             this.spr = this.spr_jump
+        end
+
+        -- Render debug throw indicator
+        -- REMOVEME eventually, should be replaced with a better indicator or hint
+
+        if this.__parent.name == 'player' then
+            love.graphics.setColor(1, 1, 1, 0.4)
+            love.graphics.line(this.x + this.w / 2, this.y + this.h / 2, this.x + this.w / 2 + 16 * math.cos(this.throw_angle), this.y + this.h / 2 + 16 * math.sin(this.throw_angle))
         end
 
         -- Apply the appropriate color.
