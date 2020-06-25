213b4dee03807d6ae5e83192ba9d6fa289278904
codeandkey <jtst@iastate.edu>
2020-06-25 11:44:59 -0500

    effect_explosion: add shrapnel effect

--- src/object_types/effect_explosion.lua
+++ src/object_types/effect_explosion.lua
@@ -3,6 +3,7 @@
 
 local object = require 'object'
 local util = require 'util'
+local map = require 'map'
 
 return {
     init = function(this)
@@ -20,8 +21,21 @@ return {
         this.smoke_dy_max = -65
         this.smoke_size_min = 4
         this.smoke_size_max = 9
-
         this.smoke_radius_max = 32
+
+        this.num_shrap_particles = this.num_shrap_particles or math.random(32, 64)
+
+        this.shrap_alpha_decay_min = 2
+        this.shrap_alpha_decay_max = 4
+        this.shrap_alpha_min = 0.5
+        this.shrap_alpha_max = 1
+        this.shrap_speed_min = 400
+        this.shrap_speed_max = 800
+        this.shrap_size_min = 8
+        this.shrap_size_max = 24
+        this.shrap_width_min = 0.2
+        this.shrap_width_max = 1
+        this.shrap_radius_max = 32
 
         this.smoke_particles = {}
         for i=1,this.num_smoke_particles do
@@ -40,6 +54,23 @@ return {
                 size = util.randrange(this.smoke_size_min, this.smoke_size_max),
             }
         end
+
+        this.shrap_particles = {}
+        for i=1,this.num_shrap_particles do
+            local ang = math.random() * 2.0 * 3.141
+            local rad = math.random() * this.shrap_radius_max
+
+            this.shrap_particles[i] = {
+                speed = util.randrange(this.shrap_speed_min, this.shrap_speed_max),
+                alpha_decay = util.randrange(this.shrap_alpha_decay_min, this.shrap_alpha_decay_max),
+                x = this.x + math.cos(ang) * rad,
+                y = this.y + math.sin(ang) * rad,
+                ang = ang,
+                alpha = util.randrange(this.shrap_alpha_min, this.shrap_alpha_max),
+                size = util.randrange(this.shrap_size_min, this.shrap_size_max),
+                width = util.randrange(this.shrap_width_min, this.shrap_width_max),
+            }
+        end
     end,
 
     update = function(this, dt)
@@ -51,6 +82,34 @@ return {
             v.x = v.x + dt * v.dx
             v.y = v.y + dt * v.dy
             v.ang = v.ang + dt * v.rotspeed
+            v.alpha = v.alpha - dt * v.alpha_decay
+
+            if v.alpha > 0 then
+                live = true
+            end
+        end
+
+        for i=1,this.num_shrap_particles do
+            local v = this.shrap_particles[i]
+
+            local x1 = v.x
+            local y1 = v.y
+
+            local x2 = v.x + math.cos(v.ang) * v.size
+            local y2 = v.y + math.sin(v.ang) * v.size
+
+            if map.aabb_tile({
+                x = x2,
+                y = y2,
+                w = 1,
+                h = 1,
+            }) then
+                v.alpha = 0
+            end
+
+            v.x = v.x + math.cos(v.ang) * dt * v.speed
+            v.y = v.y + math.sin(v.ang) * dt * v.speed
+
             v.alpha = v.alpha - dt * v.alpha_decay
 
             if v.alpha > 0 then
@@ -73,6 +132,18 @@ return {
             love.graphics.rotate(v.ang)
             love.graphics.rectangle('fill', - v.size / 2, - v.size / 2, v.size, v.size)
             love.graphics.pop()
+        end
+
+        for i=1,this.num_shrap_particles do
+            local v = this.shrap_particles[i]
+            local x1 = v.x
+            local y1 = v.y
+            local x2 = v.x + math.cos(v.ang) * v.size
+            local y2 = v.y + math.sin(v.ang) * v.size
+
+            love.graphics.setColor({1, 1, 1, v.alpha})
+            love.graphics.setLineWidth(v.width)
+            love.graphics.line({x1, y1, x2, y2})
         end
     end,
 }
