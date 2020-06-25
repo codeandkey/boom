556c8dd59b91c5ef3a59c0e7b7a07ef3d80c6327
codeandkey <jtst@iastate.edu>
2020-06-25 11:19:20 -0500

    make interactable flag use booleans

--- src/object_types/player.lua
+++ src/object_types/player.lua
@@ -161,7 +161,7 @@ return {
         -- interact prompt fade
         -- this feels.. wrong..
         map.foreach_object(function (other_obj)
-            if other_obj ~= this and other_obj.interactable == 'true' and util.aabb(char, other_obj) then
+            if other_obj ~= this and other_obj.interactable and util.aabb(char, other_obj) then
                 this.interactable = other_obj
                 this.pre_interactable = true
             end
