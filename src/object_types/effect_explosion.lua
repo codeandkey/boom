a935524deb6495405492526dc039a4571b11a22c
codeandkey <jtst@iastate.edu>
2020-06-26 02:28:14 -0500

    effect_explosion: add dirt effect

--- src/object_types/effect_explosion.lua
+++ src/object_types/effect_explosion.lua
@@ -37,6 +37,24 @@ return {
         this.shrap_width_max = 1
         this.shrap_radius_max = 32
 
+        this.num_dirt_particles = this.num_dirt_particles or math.random(32, 64)
+
+        this.dirt_alpha_decay_min = 0.3
+        this.dirt_alpha_decay_max = 0.5
+        this.dirt_rotation_min = -4
+        this.dirt_rotation_max = 4
+        this.dirt_alpha_min = 0.5
+        this.dirt_alpha_max = 1
+        this.dirt_dx_min = 0 -- can be negated if facing left
+        this.dirt_dx_max = 200
+        this.dirt_dy_min = -200
+        this.dirt_dy_max = -50
+        this.dirt_size_min = 1
+        this.dirt_size_max = 3
+        this.dirt_radius_max = 10
+        this.dirt_gravity = 156.8 -- 9.8 m/s^2 * 16 px/m
+        this.dirt_friction = 40
+
         this.smoke_particles = {}
         for i=1,this.num_smoke_particles do
             local ang = math.random() * 2.0 * 3.141
@@ -69,6 +87,29 @@ return {
                 alpha = util.randrange(this.shrap_alpha_min, this.shrap_alpha_max),
                 size = util.randrange(this.shrap_size_min, this.shrap_size_max),
                 width = util.randrange(this.shrap_width_min, this.shrap_width_max),
+            }
+        end
+
+        this.dirt_particles = {}
+        for i=1,this.num_dirt_particles do
+            local ang = math.random() * 2.0 * 3.141
+            local rad = math.random() * this.shrap_radius_max
+            local dir = 1
+
+            if math.random(2) == 1 then
+                dir = -1
+            end
+
+            this.dirt_particles[i] = {
+                rotspeed = util.randrange(this.dirt_rotation_min, this.dirt_rotation_max),
+                dx = util.randrange(this.dirt_dx_min, this.dirt_dx_max) * dir,
+                dy = util.randrange(this.dirt_dy_min, this.dirt_dy_max),
+                alpha_decay = util.randrange(this.dirt_alpha_decay_min, this.dirt_alpha_decay_max),
+                x = this.x + math.cos(ang) * rad,
+                y = this.y + math.sin(ang) * rad,
+                ang = ang,
+                alpha = util.randrange(this.dirt_alpha_min, this.dirt_alpha_max),
+                size = util.randrange(this.dirt_size_min, this.dirt_size_max),
             }
         end
     end,
@@ -114,6 +155,75 @@ return {
             end
         end
 
+        for i=1,this.num_dirt_particles do
+            local v = this.dirt_particles[i]
+
+            -- perform light aabb collisions
+            local hbox = {
+                x = v.x + dt * v.dx,
+                y = v.y,
+                w = 1,
+                h = 1,
+            }
+
+            local collision, crect = map.aabb_tile(hbox)
+
+            if collision then
+                if v.dx < 0 then
+                    v.x = crect.x + crect.w + 1
+                elseif v.dx > 0 then
+                    v.x = crect.x - 1
+                end
+
+                v.dx = 0
+                v.rotspeed = 0
+
+                if v.dy > 0 then
+                    v.dy = math.max(v.dy - dt * this.dirt_friction, 0)
+                else
+                    v.dy = math.min(v.dy + dt * this.dirt_friction, 0)
+                end
+            else
+                v.x = v.x + dt * v.dx
+            end
+
+            local vbox = {
+                x = v.x,
+                y = v.y + dt * v.dy,
+                w = 1,
+                h = 1,
+            }
+
+            local collision, crect = map.aabb_tile(vbox)
+
+            if collision then
+                if v.dy > 0 then
+                    v.y = crect.y - 1
+                elseif v.dy < 0 then
+                    v.y = crect.y + crect.h + 1
+                end
+
+                v.dy = 0
+                v.rotspeed = 0
+
+                if v.dx > 0 then
+                    v.dx = math.max(v.dx - dt * this.dirt_friction, 0)
+                else
+                    v.dx = math.min(v.dx + dt * this.dirt_friction, 0)
+                end
+            else
+                v.y = v.y + dt * v.dy
+            end
+
+            v.dy = v.dy + dt * this.dirt_gravity
+            v.ang = v.ang + dt * v.rotspeed
+            v.alpha = v.alpha - dt * v.alpha_decay
+
+            if v.alpha > 0 then
+                live = true
+            end
+        end
+
         if not live then
             object.destroy(this)
         end
@@ -125,7 +235,7 @@ return {
 
             love.graphics.push()
             love.graphics.setColor({1, 1, 1, v.alpha})
-            love.graphics.translate(v.x + v.size / 2, v.y + v.size / 2)
+            love.graphics.translate(v.x, v.y)
             love.graphics.rotate(v.ang)
             love.graphics.rectangle('fill', - v.size / 2, - v.size / 2, v.size, v.size)
             love.graphics.pop()
@@ -141,6 +251,17 @@ return {
             love.graphics.setColor({1, 1, 1, v.alpha})
             love.graphics.setLineWidth(v.width)
             love.graphics.line({x1, y1, x2, y2})
+        end
+
+        for i=1,this.num_dirt_particles do
+            local v = this.dirt_particles[i]
+
+            love.graphics.push()
+            love.graphics.setColor({v.alpha, v.alpha, v.alpha, v.alpha})
+            love.graphics.translate(v.x, v.y)
+            love.graphics.rotate(v.ang)
+            love.graphics.rectangle('fill', - v.size / 2, - v.size / 2, v.size, v.size)
+            love.graphics.pop()
         end
     end,
 }
