045ef87c79009234179295bbf6de83252dff2473
codeandkey <jtst@iastate.edu>
2020-06-29 11:15:41 -0500

    remove old options file

--- src/assets/options.lua
+++
@@ -1,9 +0,0 @@
---[[
-    boom options file "assets/options"
-    generated on 11 June 2019 at 21:00:24
-    Please use the in-game option menu to change these values!
-]]--
-
-return {
-    fullscreen = false,
-}
